387d28ed4014f542d21ecdf0953f30da28e4ed76
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-05-02 14:57:40 +0100

    restructure

--- src/main/java/com/ips/pax/MainActivity.java
+++ src/main/java/com/ips/pax/MainActivity.java
@@ -1,29 +1,21 @@
-package com.example.paxterminal.test_ped;
+package com.ips.pax;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.*;
+import android.content.pm.*;
 import android.net.Uri;
-import android.os.ParcelFileDescriptor;
+import android.os.*;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
+
+import java.lang.Process;
 import java.util.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.*;
+import java.io.*;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-
+import static com.ips.pax.BuildConfig.APPLICATION_ID;
 
 public class MainActivity extends AppCompatActivity {
 private Button getConfigButton;
@@ -32,11 +24,15 @@ private Button refundButton;
 private TextView logField;
 private EditText amountField;
 private EditText transactionReferenceField;
+private TextView runtimeLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogReaderTask logReaderTask = new LogReaderTask();
+        logReaderTask.execute();
+
         getConfigButton = findViewById(R.id.getConfigButton);
         getConfigButton.setOnClickListener(configButtonListener());
         paymentButton = findViewById(R.id.paymentButton);
@@ -44,8 +40,10 @@ private EditText transactionReferenceField;
         refundButton = findViewById(R.id.refundButton);
         refundButton.setOnClickListener(refundButtonListener());
         logField = findViewById(R.id.logField);
+        runtimeLog = findViewById(R.id.runtimeLog);
         amountField = findViewById(R.id.amountField);
         transactionReferenceField = findViewById(R.id.transactionReference);
+
     }
     private View.OnClickListener configButtonListener(){
         return new View.OnClickListener() {
@@ -67,6 +65,7 @@ private EditText transactionReferenceField;
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("System","starting redund");
                 Transaction("REFUND");
             }
         };
@@ -76,6 +75,7 @@ private EditText transactionReferenceField;
         mRequestFileIntent.setType("application/json");
         mRequestFileIntent.setPackage("com.paxitalia.cb2.xxx");
         if (isIntentAvailable(getApplicationContext(),mRequestFileIntent)){
+            Log.i("getconfig intent","starting getconfig intent");
             startActivityForResult(mRequestFileIntent, 0);
         }else{
             logField.setText("Intent Not Available Dude.. !");
@@ -84,6 +84,7 @@ private EditText transactionReferenceField;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i("getconfig intent","resultcode: "+resultCode+ " result : "+ data + " ->" +data.getData());
         do{
             if(resultCode != RESULT_OK){
                 break;
@@ -165,7 +166,7 @@ private EditText transactionReferenceField;
         builder = new Uri.Builder();
         builder.scheme("pax")
                 .authority("payment")
-                .appendQueryParameter("callerPackage", BuildConfig.APPLICATION_ID)
+                .appendQueryParameter("callerPackage",  APPLICATION_ID)
                 .appendQueryParameter("paymentType", transactionType)
                 .appendQueryParameter("amount", amountField.getText().toString())
                 .appendQueryParameter("callerTrxId", transactionReferenceField.getText().toString())
@@ -174,14 +175,19 @@ private EditText transactionReferenceField;
         Intent intent = new Intent(Intent.ACTION_VIEW, builder.build());
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (isIntentAvailable(getApplicationContext(),intent)){
+            Log.i("transaction intent","starting transaction for "+transactionType);
                 startActivity(intent);
-                if (getIntent() != null && getIntent().getData() != null && getIntent().getData().getHost() != null /**&& getIntent().getData().getHost().equals("PAYMENT_HOST")**/) {
+                Log.i("transaction intent","started transaction for "+transactionType);
+                if (getIntent() != null && getIntent().getData() != null && getIntent().getData().getHost() != null  /**&& getIntent().getData().getHost().equals("PAYMENT_HOST")**/) {
+                    Log.i("transaction intent","got intent");
+                    Log.i("transaction intent","intent -> "+getIntent() + " data-> " + getIntent().getData());
                     Uri data = getIntent().getData();
                     String callTrx = data.getQueryParameter("callerTrxId");
                     String result = data.getQueryParameter("result");
                     String message = data.getQueryParameter("message");
                     String optMessage = data.getQueryParameter("optMessage");
                     if (!TextUtils.isEmpty(result)) {
+                        Log.i("transaction intent","Result " + result + " , message: " + message + " , opt.message: " + optMessage + " , callerTrxId = " + callTrx);
                         logField.setText("Result " + result + " , message: " + message + " , opt.message: " + optMessage + " , callerTrxId = " + callTrx);
                     }
                 }
@@ -197,4 +203,83 @@ private EditText transactionReferenceField;
                         PackageManager.MATCH_DEFAULT_ONLY);
         return list.size() > 0;
     }
+
+
+
+    private class LogReaderTask extends AsyncTask<Void, String, Void> {
+        private final String[] LOGCAT_CMD = new String[] { "logcat" };
+        private final int BUFFER_SIZE = 1024;
+
+        private boolean isRunning = true;
+        private Process logprocess = null;
+        private BufferedReader reader = null;
+        private String[] line = null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                logprocess = Runtime.getRuntime().exec(LOGCAT_CMD);
+            } catch (IOException e) {
+                e.printStackTrace();
+
+                isRunning = false;
+            }
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(
+                        logprocess.getInputStream()),BUFFER_SIZE);
+            }
+            catch(IllegalArgumentException e){
+                e.printStackTrace();
+
+                isRunning = false;
+            }
+
+            line = new String[1];
+
+            try {
+                while(isRunning)
+                {
+                    line[0] = reader.readLine();
+
+                    publishProgress(line);
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+
+                isRunning = false;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            runtimeLog.append(values[0]);
+        }
+
+        public void stopTask(){
+            isRunning = false;
+            logprocess.destroy();
+        }
+    }
 }
+
